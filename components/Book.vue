a569df9500a5efc217115e3ebf256f8371bf8b02
Shemmy <shemmynyirenda3@gmail.com>
2025-05-12 13:27:03 +0200

    fixed

--- components/Book.vue
+++ components/Book.vue
@@ -7,19 +7,18 @@ const props = defineProps({
 
 <template>
   <li
-    class="w-full h-full flex flex-col justify-center items-center cursor-pointer overflow-clip transition-all duration-300 hover:opacity-55"
+    class="w-full h-full flex flex-col cursor-pointer overflow-clip transition-all duration-300 hover:opacity-55"
   >
-    <img
-      v-if="props.cover"
-      class="rounded-xl bg-colorSurface-light dark:bg-colorSurface-dark text-center text-sm w-32 h-48 object-cover"
-      :src="props.cover"
-      :alt="props.title"
-    />
     <div
-      v-else
-      class="w-32 h-48 bg-colorSurface-light dark:bg-colorSurface-dark flex items-center justify-center rounded-xl text-sm opacity-70"
+      class="flex items-center justify-center rounded-xl bg-colorSurface-light dark:bg-colorSurface-dark text-center text-sm w-full flex-grow-0 aspect-[8/12]"
     >
-      No Cover
+      <img
+        v-if="props.cover"
+        :src="props.cover"
+        :alt="props.title"
+        class="w-full h-full object-cover rounded-xl"
+      />
+      <p v-else>No Cover</p>
     </div>
     <p class="mt-2 py-1 px-2 font-bold text-xs">{{ props.title }}</p>
   </li>
